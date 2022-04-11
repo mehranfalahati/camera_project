995df27d339cfec6efaa53bbbab1676a31b256d7
Mehran Falahati <mehran_falahati@yahoo.com>
2022-04-11 14:50:26 +1000

    camerahouse

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:new, :create]
 
-  get '/login' => 'session'
+  get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
 end
